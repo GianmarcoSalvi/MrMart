0ffb28fe4a277740b975f37af75d0a1d7f8720d8
GianmarcoSalvi <gianmarco10.salvi@live.it>
2019-09-04 19:06:47 +0200

    modifiche

--- app/src/main/java/com/example/mytrueapplication/CatergorieSupermercati.java
+++ app/src/main/java/com/example/mytrueapplication/CatergorieSupermercati.java
@@ -1,9 +1,12 @@
 package com.example.mytrueapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.mytrueapplication.R;
 
@@ -13,6 +16,9 @@ public class CatergorieSupermercati extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catergorie_supermercati);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
 
@@ -20,6 +26,20 @@ public class CatergorieSupermercati extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.ricerca_cart_prezzo, menu);
+        return true;
+    }
+
+
+    // tasto back nuova activity
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
         return true;
     }
 }
